17b32ef5ebbc7ef00d0886cb048a1faca44f0302
shanty <1409149045@qq.com>
2020-06-13 08:16:29 +0800

    解决提交冲突

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-import os
-print(os.listdir('./pth'))
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+torch.device
+plt.scatter(np.asarray([0,100])+100,np.asarray([0,100])+100)
+plt.plot(np.asarray([0,100]),np.asarray([0,100])+100)
+plt.legend(('af',' bf'), loc='upper right')
+plt.show()
